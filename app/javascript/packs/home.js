df5ed6b8801b6bc24807d64f9ef2bac8e5f6c887
Zogoo <chtsogbadrakh@gmail.com>
2020-05-14 00:39:27 +0900

    Add basic UI with structure

---
+++ app/javascript/packs/home.js
@@ -0,0 +1,19 @@
+import Vue from "vue";
+import Vuetify from "vuetify";
+import "vuetify/dist/vuetify.min.css";
+import App from "../App.vue";
+import router from "../router";
+import i18n from "../i18n";
+
+Vue.use(Vuetify);
+const vuetify = new Vuetify();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const app = new Vue({
+    router,
+    i18n,
+    vuetify,
+    render: (h) => h(App),
+  }).$mount();
+  document.body.appendChild(app.$el);
+});
