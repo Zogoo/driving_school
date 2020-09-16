31e46b1f22b53f05acf22b58e277d6e4c72854f6
Zogoo <chtsogbadrakh@gmail.com>
2020-09-16 23:02:17 +0900

    Redirect back to original request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,6 @@
 class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
-    if resource.teacher.present?
-      '/teacher/my_page'
-    else
-      '/student/my_page'
-    end
+    stored_location_for(resource) || resource.teacher.present? ? '/teacher/my_page' : '/student/my_page'
   end
 end
